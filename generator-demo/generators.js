d0c357ac93d7a2e02c401537d69f61472ac04544
Chakrit Likitkhajorn <chakrit.lj@gmail.com>
2019-05-30 22:11:59 +0700

    WIP

--- generator-demo/generators.js
+++ generator-demo/generators.js
@@ -7,9 +7,11 @@ function* generator() {
 
 function run() {
   const gen = generator()
+  console.log(gen.next()) // Get first number from line 2 (yield 1)
+  console.log(gen.next(123)) // Send 123 to i in line 2, and get new value (which is yield i in line 3)
+  console.log(gen.next(3)) // Send 3 to y in line 3, and get a new value (which is return 5). In this line done is true
   console.log(gen.next())
-  console.log(gen.next(123))
-  console.log(gen.next(3))
+  console.log(gen.next())
 }
 
 run()
